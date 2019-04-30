c279e2ed92174b3252e69837a2addb3bc1653da7
Memoona Mughal <mem.mughal+github1@gmail.com>
2019-04-30 11:13:51 +0200

    Add libgraph library

--- mix.exs
+++ mix.exs
@@ -36,7 +36,8 @@ defmodule JobProcessing.MixProject do
       {:phoenix_pubsub, "~> 1.1"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:libgraph, "~> 0.7"}
     ]
   end
 end
